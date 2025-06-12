c9b5514d4bc8045f2d2aaf4f09ba078dad7e4141
Gabriel Ribeiro <gabrielribeirotnc@gmail.com>
2025-06-12 16:04:21 +0000

    refector: fix identation

--- src/js/script.js
+++ src/js/script.js
@@ -1,60 +1,51 @@
-const form = document.querySelector('.form-inputs');
-const taskInput = document.querySelector('.taskInput');
-const taskList = document.querySelector('.taskList'); // UL
-const btnAdd = document.querySelector('.btnAdd');
+const form = document.querySelector(".form-inputs");
+const taskInput = document.querySelector(".taskInput");
+const taskList = document.querySelector(".taskList"); // UL
+const btnAdd = document.querySelector(".btnAdd");
 
 // let tasks = JSON.parse(localStorage.getItem("listTasks",)) || []; // recebendo valores armazenados no localstorage ou uma array
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const currentTask = {
-        name: taskInput.value.toUpperCase(),
-        done: false,
-        edit: false,
-        id: Math.floor(Math.random() * 10000)
-    }
-    addTask(currentTask);
-    showTask();
-
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const currentTask = {
+    name: taskInput.value.toUpperCase(),
+    done: false,
+    edit: false,
+    id: Math.floor(Math.random() * 10000),
+  };
+  addTask(currentTask);
+  showTask();
 });
 
 function addTask(task) {
-    let tasks = getTasks();
-    tasks.push(task);
-    localStorage.setItem("listTasks", JSON.stringify(tasks));
+  let tasks = getTasks();
+  tasks.push(task);
+  localStorage.setItem("listTasks", JSON.stringify(tasks));
 }
 
 function showTask() {
-    const tasks = getTasks();
-    taskList.innerHTML = '';
+  const tasks = getTasks();
+  taskList.innerHTML = "";
 
-    tasks.forEach((task) => {
-        const li = document.createElement('li');
-        li.className = 'liTask';
-        li.innerHTML = `<span>${task.name}</span> <button onclick="btnDelete(${task.id})">delete</button>`
-        taskList.appendChild(li);
-
-
-    }
-    )
-
+  tasks.forEach((task) => {
+    const li = document.createElement("li");
+    li.className = "liTask";
+    li.innerHTML = `<span>${task.name}</span> <button class="btnDelete" onclick="btnDelete(${task.id})">delete</button>`;
+    taskList.appendChild(li);
+  });
 }
 
 function btnDelete(id) {
-    const tasks = getTasks();
-    const newTasks = tasks.filter((task) => task.id !== id);
-    console.log(newTasks);
-    setTasks(newTasks);
-    showTask();
-
+  const tasks = getTasks();
+  const newTasks = tasks.filter((task) => task.id !== id);
+  setTasks(newTasks);
+  showTask();
 }
 
 function setTasks(tasks) {
-    localStorage.setItem("listTasks", JSON.stringify(tasks));
+  localStorage.setItem("listTasks", JSON.stringify(tasks));
 }
-
 
 function getTasks() {
-    return JSON.parse(localStorage.getItem("listTasks")) || [];
+  return JSON.parse(localStorage.getItem("listTasks")) || [];
 }
-
