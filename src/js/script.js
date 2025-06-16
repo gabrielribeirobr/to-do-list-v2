59040d577496bf8fac6f7b312c131bff30ad808d
Gabriel Ribeiro <gabrielribeirotnc@gmail.com>
2025-06-16 16:05:17 +0000

    refector: function btnSave works

--- src/js/script.js
+++ src/js/script.js
@@ -33,16 +33,21 @@ function showTask() {
     li.className = "liTask";
     li.id = task.id;
     li.innerHTML = `<span>${task.name}</span>  
-    <input type="text" class="input" name="input edit" value= "${task.name}">
+    <input type="text" class="input input--edit" name="input edit" value= "${task.name}">
     <div class="boxButtons"> 
     <button class="button edit" onclick="btnEdit(${task.id})"><i class="fa-regular fa-pen-to-square"></i></button> 
     <button class="button delete" onclick="btnDelete(${task.id})"><i class="fa-regular fa-trash-can"></i></button>   
     <button class="button save" onclick="btnSave(${task.id})">salvar</button>
-    <button class="button cancel" onclick="btnCancel()">Cancelar</button>
+    <button class="button cancel" onclick="btnCancel(${task.id})">Cancelar</button>
     </div>
     `;
     taskList.appendChild(li);
   });
+}
+
+function btnCancel(id){
+  const currentLi = document.getElementById(id);
+  currentLi.classList.remove('editing');
 }
 
 function btnEdit(id) {
@@ -51,8 +56,21 @@ function btnEdit(id) {
 }
 
 function btnSave(id) {
+  const tasks = getTasks();
+  const task = tasks.find((t) => t.id === id); // finding id for currentTask
+
   const currentLi = document.getElementById(id);
   currentLi.classList.remove("editing");
+
+  const input = currentLi.querySelector(".input--edit");
+  const taskEdited = input.value;
+  
+  if (taskEdited) {
+    task.name = taskEdited;
+    setTasks(tasks);
+    showTask(); 
+  }
+   
 }
 
 function btnDelete(id) {
